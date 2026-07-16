cb03b5a50bdc2dd4315fbca1483af2ed7ace2340
OleksaDudko <132586603+OleksaDudko@users.noreply.github.com>
2026-07-16 19:28:56 +0300

    img_render

--- src/main.js
+++ src/main.js
@@ -1,41 +1,56 @@
 import "./js/getEvent"
 
-// const URL = "https://app.ticketmaster.com/discovery/v2/";
-// const API_KEY = "hXUd5IDKsavTl95aAOfGkyFDSk68VDlw";
-
-// let keyword = "";
-// let country = "";
-// let page = 1;
-
-// async function getImg() {
-//     const res = await fetch(
-//         `https://app.ticketmaster.com/discovery/v2/events?apikey=hXUd5IDKsavTl95aAOfGkyFDSk68VDlw&locale=*`
-//     );
-//     console.log(res);
-    
-//     const data = await res.json();
-//     return data
-// }
-
-// getImg()
-
+const listEl = document.querySelector(".list")
 
 const URL = "https://app.ticketmaster.com/discovery/v2/events.json";
 const API_KEY = "hXUd5IDKsavTl95aAOfGkyFDSk68VDlw";
 
-async function getEvents() {
-  const res = await fetch(
-    `${URL}?apikey=${API_KEY}&locale=*`
-  );
+let keyword = "";
+let country = "";
+let page = 1;
 
-  const data = await res.json();
-  data._embedded.events.forEach(event => {
-    console.log(event.name);              // Назва
-    console.log(event.images[0].url);     // Зображення
-    console.log(event.dates.start.localDate); // Дата
-    console.log(event._embedded.venues[0].name); // Місце
-});
-  console.log(data);
+async function getEvents() {
+    const res = await fetch(
+        `${URL}?apikey=${API_KEY}&locale=*`
+    )
+    
+    const data = await res.json();
+  
+    
+    return data._embedded?.events || []
 }
 
 getEvents()
+
+
+function render(arr) {
+    const item = arr.map((e) => {
+      
+    const image = e.images[0]?.url;
+    const name = e.name;
+    const date = e.dates.start.localDate;
+    const city = e._embedded?.venues[0]?.city?.name || "Unknown";
+    return `
+        <li class="event-card">
+
+            <img src="${image}" alt="${name}" class="img">
+
+            <h2 class="name">${name}</h2>
+
+            <p class="date">${date}</p>
+
+            <p class="city">${city}</p>
+
+        </li>`
+    }).join("");
+
+    listEl.innerHTML = item;
+}
+
+async function init() {
+    const events = await getEvents();
+
+    render(events);
+}
+
+init()
